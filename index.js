8cb95c2639111fbdb536a5b05c6fac905e65fcaa
habibulloh004 <habibulloh4080@gmail.com>
2024-03-02 16:46:52 +0500

    url

--- index.js
+++ index.js
@@ -15,14 +15,16 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  // console.log(`user id: ${socket.id}`);
+  console.log(`user id: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-  });
+  // socket.on("join_room", (data) => {
+  //   socket.join(data);
+  // });
+  socket.emit("userId", socket.id)
 
   socket.on("send_message", (data) => {
-    socket.to(data.room).emit("recieve_message", data);
+    // socket.to(data.room).emit("recieve_message", data);
+    socket.broadcast.emit("recieve_message", data);
   });
   // socket.to(data.room).emit("user_id", socket.id);
 });
