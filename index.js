9c714f2e6388621437e6d1958b44332a450c943b
habibulloh004 <habibulloh4080@gmail.com>
2024-03-05 16:54:57 +0500

    hello guys2

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://localhost:3000", // Update origin to use HTTPS
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
   console.log(`user id: ${socket.id}`);
-  
+
   socket.on("send_message", (data) => {
     // socket.to(data.room).emit("recieve_message", data);
     socket.broadcast.emit("recieve_message", data);
   });
-  socket.emit("userData", socket)
+  socket.emit("userData", socket);
   // socket.to(data.room).emit("user_id", socket.id);
 });
 
