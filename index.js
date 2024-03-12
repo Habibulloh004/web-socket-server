a7087621eb7ba5d9f975a65583156262f267892e
habibulloh004 <habibulloh4080@gmail.com>
2024-03-12 14:28:17 +0500

    kuryer

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ io.on("connection", (socket) => {
 
   socket.on("join_room", (data) => socket.join(data));
   socket.on("send_message", (data) => {
-    socket.to(data.room).emit("recieve_message", data);
-    // socket.broadcast.emit("recieve_message", data);
+    // socket.to(data.room).emit("recieve_message", data);
+    socket.broadcast.emit("recieve_message", data);
     // console.log(data);
   });
 });
