d365208cd6dfbeca3e0992a15cffaac263957bd2
habibulloh004 <habibulloh4080@gmail.com>
2024-03-16 18:22:28 +0500

    qewrrewq122

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const io = new Server(server, {
   cors: {
     origin: [
       "*",
-      "http://localhost:3002",
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "https://localhost:3000",
       "http://joinposter.com",
       "https://morella-ice.vercel.app/",
       "https://joinposter.com",
