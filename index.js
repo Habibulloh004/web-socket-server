f0af1d3fe002eb98aec1b668f25245387fefe7e0
habibulloh004 <habibulloh4080@gmail.com>
2024-03-16 15:13:24 +0500

    qewrrewq

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const io = new Server(server, {
   cors: {
     origin: [
       "*",
-      // "http://localhost:3002",
-      // "http://localhost:3000",
-      // "https://localhost:3000",
+      "http://localhost:3002",
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://localhost:3000",
       "http://joinposter.com",
       "https://morella-ice.vercel.app/",
       "https://joinposter.com",
@@ -32,8 +33,8 @@ io.on("connection", (socket) => {
 
   socket.on("join_room", (data) => socket.join(data));
   socket.on("send_message", (data) => {
-    // socket.to(data.room).emit("recieve_message", data);
-    socket.broadcast.emit("recieve_message", data);
+    socket.to(data.user).emit("recieve_message", data);
+    // socket.broadcast.emit("recieve_message", data);
     console.log(data);
   });
 });
